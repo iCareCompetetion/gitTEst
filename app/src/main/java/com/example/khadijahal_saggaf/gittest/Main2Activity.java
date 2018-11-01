8a201e99c554bdcf66bbb757c4cee152d08b28e9
Aeysha Ibrahim Mahmoud <ayoosh0056@gmail.com>
2018-11-02 00:27:44 +0300

    Add new Activity , declaring a button

--- app/src/main/java/com/example/khadijahal_saggaf/gittest/Main2Activity.java
+++ app/src/main/java/com/example/khadijahal_saggaf/gittest/Main2Activity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -15,6 +16,8 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Button b = (Button) findViewById(R.id.button);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
