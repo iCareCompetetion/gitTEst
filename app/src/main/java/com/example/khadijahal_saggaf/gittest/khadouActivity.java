a0fbbdb14ae9b4886e86552105670298cc986b5e
KhadijahAl_Saggaf <ksakkaf@stu.kau.edu.sa>
2018-11-02 00:32:21 +0300

    i delete activity2

---
+++ app/src/main/java/com/example/khadijahal_saggaf/gittest/khadouActivity.java
@@ -0,0 +1,15 @@
+package com.example.khadijahal_saggaf.gittest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class khadouActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_khadou);
+
+        //here i ill add commit
+    }
+}
